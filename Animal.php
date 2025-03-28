836713ac598c563adac89d929c608d878899d1e3
djimemez <djimemez@xtec.cat>
2025-03-28 19:05:00 +0100

    Nueva classe Animal

---
+++ Animal.php
@@ -0,0 +1,12 @@
+<?php 
+class Animal{
+    private $nom;
+    private $edat;
+
+    public function __construct($nomP, $edatP){
+        $this->nom = $nomP;
+        $this->edat = $edatP;
+    }
+}
+
+?>
